0c461b07bc0e3353c636205f25af6dda105ca5f2
Ben Butler <benbutler08@gmail.com>
2018-05-08 14:18:14 -0400

    New controllers and partials
    Create a relationships controller and a users controller. Add a show_follow partial to display the followers of a given user. Create a follow and unfollow partial to display in a users 'card'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   resources :ratings
   resources :restaurants
+  resources :relationships, only: [:create, :destroy]
   root 'home#feed'
   get 'users_feed', to: 'home#users'
   get 'following', to: 'home#now_following'
   get 'restaurants_feed', to: 'home#restaurants'
   devise_for :users
+  resources :users do
+    member do
+      get :followers, :following
+    end
+  end
 end
