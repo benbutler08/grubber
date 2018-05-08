09cf7c5403d93001f2f8cde048ed02f7349efa5e
Ben Butler <benbutler08@gmail.com>
2018-05-08 11:10:38 -0400

    Update scaffold styling
    Change the scaffolds to show tables and forms from the material library.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.create(email: 'ben@example.com', password: 'asdf123', password_confirmation: 'asdf123')
 Restaurant.create(name: 'Bankers Hill', location: 'San Diego, CA')
 Restaurant.create(name: 'Eats', location: 'Providence, RI')
 Restaurant.create(name: 'Amelies', location: 'Charlotte, NC')
+Rating.create(title: 'test rating', score: 4, description: 'I went here and it was good', restaurant_id: 1, user_id: 1)
