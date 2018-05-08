9e0a70dc8724e97b5929636df52039e2647c2149
Ben Butler <benbutler08@gmail.com>
2018-05-08 06:46:14 -0400

    update home routes
    Change home routes to _feed routes. They were conflicting with resource routes from the new restaurants controller.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :ratings
   resources :restaurants
   root 'home#feed'
-  get 'home', to: 'home#users'
-  get 'restaurants', to: 'home#restaurants'
+  get 'users_feed', to: 'home#users'
+  get 'restaurants_feed', to: 'home#restaurants'
   devise_for :users
 end
