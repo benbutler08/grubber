fe785a6e7279eb825d2575d37ddb1347281b3b64
Ben Butler <benbutler08@gmail.com>
2018-05-09 12:01:14 -0400

    Update following actions
    Change the following actions to the user show pages. See comments in the users feed view that corresponds with the users action in the home controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:email, :avatar])
   end
-  
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,11 +20,6 @@ restaurant.save!
 
 
 User.create(email: 'ben@example.com', username: 'SuperUserBen',password: 'asdf123', password_confirmation: 'asdf123')
-Restaurant.create(name: 'Bankers Hill', location: 'San Diego, CA')
-Restaurant.create(name: 'Eats', location: 'Providence, RI')
-Restaurant.create(name: 'Amelies', location: 'Charlotte, NC')
-
-
 Rating.create(title: 'test rating', score: 4, description: 'I went here and it was good', restaurant_id: 1, user_id: 1)
 
 99.times do |n|
