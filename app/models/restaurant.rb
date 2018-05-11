a8ce4191dc0e1e9d7aeee979fed37c9b55c2f3e8
Ben Butler <benbutler08@gmail.com>
2018-05-11 10:41:39 -0400

    fix zero division error
    It was trying to divide by zero when there were no ratings

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,10 +3,14 @@ class Restaurant < ApplicationRecord
   mount_uploader :restaurant_pic, RestaurantPicUploader
 
   def find_average_rating
-    sum_of_ratings = [0]
-      ratings.each do |rating|
-      sum_of_ratings.push(rating.score)
+    if ratings.count > 0
+      sum_of_ratings = []
+        ratings.each do |rating|
+        sum_of_ratings.push(rating.score)
+      end
+      averaged_rating = sum_of_ratings.inject(:+)/(sum_of_ratings.length)
+    else
+      0
     end
-    averaged_rating = sum_of_ratings.inject(:+)/(sum_of_ratings.length - 1 )
   end
 end
